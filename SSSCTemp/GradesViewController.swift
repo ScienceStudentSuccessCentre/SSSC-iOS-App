6ace76334e33c4b642ce41acf49fb9c92ea5d8f0
Avery Vine <averyvine@icloud.com>
2018-09-27 23:48:41 -0400

    added base layout for grades tab and started work on terms table

--- SSSCTemp/GradesViewController.swift
+++ SSSCTemp/GradesViewController.swift
@@ -8,11 +8,37 @@
 import UIKit
 
 class GradesViewController: UIViewController {
+    
+    @IBOutlet var segmentControl: UISegmentedControl!
+    @IBOutlet var termsView: UIView!
+    @IBOutlet var calculatorView: UIView!
+    @IBOutlet var plannerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 25),NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        calculatorView.isHidden = true
+        plannerView.isHidden = true
+    }
+    
+    @IBAction func segmentSelectedAction(sender: AnyObject) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            termsView.isHidden = false
+            calculatorView.isHidden = true
+            plannerView.isHidden = true
+        case 1:
+            termsView.isHidden = true
+            calculatorView.isHidden = false
+            plannerView.isHidden = true
+        case 2:
+            termsView.isHidden = true
+            calculatorView.isHidden = true
+            plannerView.isHidden = false
+        default:
+            break
+        }
     }
     
 
---
+++ SSSCTemp/TermsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TermsViewController.swift
+//  SSSCTemp
+//
+//  Created by Avery Vine on 2018-09-27.
+//  Copyright © 2018 Avery Vine. All rights reserved.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
