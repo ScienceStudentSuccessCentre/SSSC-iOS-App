6f05f2d6a3a29b0d1b0e07dafdbaa46e77316d0e
Avery Vine <averyvine@icloud.com>
2019-11-07 17:55:44 -0500

    added mentors to mentoring page

---
+++ ScienceStudentSuccessCentre/Mentoring/MentorCell.swift
@@ -0,0 +1,69 @@
+//
+//  MentorCell.swift
+//  ScienceStudentSuccessCentre
+//
+//  Created by Avery Vine on 2019-11-07.
+//  Copyright © 2019 Avery Vine. All rights reserved.
+//
+
+import UIKit
+
+class MentorCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var degree: UILabel!
+    var loadedImage = false
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        imageView.layer.masksToBounds = true
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        name.text = nil
+        degree.text = nil
+        changeImage(to: nil, animated: false)
+    }
+    
+    func configure(_ mentor: Mentor) {
+        name.text = mentor.getName()
+        degree.text = mentor.getDegree()
+        loadImage(url: mentor.getImageUrl())
+    }
+    
+    private func loadImage(url: URL?) {
+        if let url = url {
+            DispatchQueue.global().async {
+                if let data = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        self.changeImage(to: UIImage(data: data))
+                    }
+                }
+            }
+        }
+    }
+    
+    private func changeImage(to image: UIImage?, animated: Bool = true) {
+        if animated {
+            UIView.transition(with: imageView,
+                              duration: 0.2,
+                              options: .transitionCrossDissolve,
+                              animations: {
+                                if #available(iOS 13.0, *) {
+                                    self.imageView.image = image ?? UIImage(systemName: "person.crop.circle")
+                                } else {
+                                    self.imageView.image = image
+                                }
+                                self.loadedImage = image != nil
+            })
+        } else {
+            if #available(iOS 13.0, *) {
+                imageView.image = image ?? UIImage(systemName: "person.crop.circle")
+            } else {
+                imageView.image = image
+            }
+            loadedImage = image != nil
+        }
+    }
+}
---
+++ ScienceStudentSuccessCentre/Mentoring/MentorLoader.swift
@@ -0,0 +1,73 @@
+//
+//  MentorLoader.swift
+//  ScienceStudentSuccessCentre
+//
+//  Created by Avery Vine on 2019-11-07.
+//  Copyright © 2019 Avery Vine. All rights reserved.
+//
+
+import Foundation
+import PromiseKit
+
+/// Utility class that retrieves SSSC mentors from the server and parses them into proper `Mentor` objects.
+class MentorLoader {
+    private static let serverUrl = URL(string: "http://sssc-carleton-app-server.herokuapp.com/mentors")
+    
+    /// Gathers mentor data from the server and passes it on to be deserialized.
+    ///
+    /// - Returns: The newly parsed mentors in the form of a promise.
+    static func loadMentors() -> Promise<[Mentor]> {
+        return Promise { seal in
+            if let url = serverUrl {
+                URLSession.shared.dataTask(with: url) { result in
+                    switch result {
+                    case .success(_, let data):
+                        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                            seal.reject(URLError(.badServerResponse))
+                            return
+                        }
+                        let mentors = deserializeMentors(json: json)
+                        let sortedMentors = sortMentors(mentors)
+                        seal.fulfill(sortedMentors)
+                    case .failure(let error):
+                        seal.reject(error)
+                    }
+                }.resume()
+            } else {
+                seal.reject(URLError(.badURL))
+            }
+        }
+    }
+    
+    /// Converts JSON data into a list of SSSC mentors.
+    ///
+    /// - Parameter json: JSON data retrieved from the server.
+    /// - Returns: The list of mentors, parsed and sorted.
+    private static func deserializeMentors(json: Any) -> [Mentor] {
+        var mentors = [Mentor]()
+        if let jsonMentors = json as? NSArray {
+            for jsonMentor in jsonMentors {
+//                print(jsonMentors)
+                if let mentorData = jsonMentor as? NSDictionary,
+                    let mentor = Mentor(mentorData: mentorData) {
+                    mentors.append(mentor)
+                } else {
+                    print("Failed to generate mentor...")
+                }
+            }
+        } else {
+            print("JSON data is invalid")
+        }
+        return mentors
+    }
+    
+    /// Sorts the list of SSSC mentors into alphabetical order by first name.
+    ///
+    /// - Parameter mentors: The list of mentors to sort.
+    /// - Returns: The list of mentors in alphabetical order.
+    private static func sortMentors(_ mentors: [Mentor]) -> [Mentor] {
+        return mentors.sorted {
+            $0.getName() < $1.getName()
+        }
+    }
+}
